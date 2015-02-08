4fb5e6739aa526f6a65d2461137dcbc266544643
AIZAWA, Hina <hina@bouhime.com>
2015-02-09 03:54:40 +0900

    Calc TOTP

--- src/Totp.php
+++ src/Totp.php
@@ -7,6 +7,8 @@
  */
 
 namespace jp3cki\totp;
+
+use InvalidArgumentException;
 use Base32\Base32;
 
 /**
@@ -22,9 +24,8 @@ class Totp {
     /** Default digits: 6 digits */
     const DEFAULT_DIGITS = 6;
 
-    /** Default interval: 30 sec */
-    const DEFAULT_INTERVAL_SEC = 30;
-
+    /** Default time step: 30 sec */
+    const DEFAULT_TIME_STEP_SEC = 30;
 
     /** 
      * Generate user key
@@ -43,5 +44,85 @@ class Totp {
             throw new \Exception('System does not support strong random generating');
         }
         return Base32::encode($binary);
+    }
+
+    /**
+     * Calculate TOTP
+     *
+     * @param   string          $key        Base32 encoded key
+     * @param   int|\DateTime   $time       A value that reflects a time
+     * @param   int             $digits     Number of digits to return
+     * @param   string          $hash       Hash algorithm such as "sha1", "sha256" or "sha512"
+     * @param   int             $time_step  Time-step
+     * @return  string                      TOTP value like "012345"
+     * @throws  \InvalidArgumentException   Throw exception if not-acceptable parameter given.
+     */
+    public static function calc(
+        $key,
+        $time,
+        $digits = self::DEFAULT_DIGITS,
+        $hash = self::DEFAULT_HASH_ALGORITHM,
+        $time_step = self::DEFAULT_TIME_STEP_SEC
+    ) {
+        if(!preg_match('/^([A-Z2-7]+)=*$/', $key, $match)) {
+            throw new InvalidArgumentException("Invalid shared secret key given");
+        } else {
+            $key = strtoupper($match[1]);
+        }
+        if(is_int($time)) {
+            // ok
+        } elseif($time instanceof \DateTime) {
+            $time = $time->getTimestamp();
+        } elseif(is_numeric($time)) {
+            $time = (int)$time;
+        } else {
+            throw new InvalidArgumentException("Invalid timestamp given");
+        }
+        if(is_int($digits) || is_numeric($digits)) {
+            $digits = (int)$digits;
+            if($digits < 1 || $digits > 8) {
+                throw new InvalidArgumentException("Digit-of-return value is out of range");
+            }
+        } else {
+            throw new InvalidArgumentException("Invalid digits-of-return given");
+        }
+        $hash = strtolower($hash);
+        if(!in_array($hash, hash_algos(), true)) {
+            throw new InvalidArgumentException("Unsupported hash algorithm");
+        }
+        if(is_int($time_step) || is_numeric($time_step)) {
+            $time_step = (int)$time_step;
+            if($time_step < 1) {
+                throw new InvalidArgumentException("Time-step value is out of range");
+            }
+        }
+
+        $key_binary = Base32::decode($key);
+        $t = self::pack64((int)floor($time / $time_step));
+        $hmac = hash_hmac($hash, $t, $key_binary, true);
+        $offset = ord($hmac[strlen($hmac) - 1]) & 0x0f;
+        $int_value = ((ord($hmac[$offset]) & 0x7f) << 24) +
+                     ((ord($hmac[$offset + 1])) << 16) +
+                     ((ord($hmac[$offset + 2])) << 8) +
+                     ((ord($hmac[$offset + 3])) << 0);
+        $otp = (string)($int_value % pow(10, $digits));
+        return substr(str_repeat('0', $digits) . $otp, -$digits);
+    }
+
+    /**
+     * Pack 64bit integer to bigendian binary
+     *
+     * @param int $value int64 value
+     */
+    private static function pack64($value) {
+        if(version_compare(PHP_VERSION, '5.6.3', '>=')) {
+            return pack('J', $value);
+        } else {
+            $high_map = 0xffffffff << 32;
+            $low_map  = 0xffffffff;
+            $higher = ($value & $high_map) >> 32; 
+            $lower = $value & $low_map; 
+            return pack('NN', $higher, $lower); 
+        }
     }
 }
--- test/TotpTest.php
+++ test/TotpTest.php
@@ -1,5 +1,6 @@
 <?php
 use jp3cki\totp\Totp;
+use Base32\Base32;
 
 class TotpTest extends \PHPUnit_Framework_TestCase {
     public function testGenerateChars() {
@@ -12,5 +13,90 @@ class TotpTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals(16, strlen(Totp::generateKey( 80)));
         $this->assertEquals(24, strlen(Totp::generateKey(120)));
         $this->assertEquals(32, strlen(Totp::generateKey(160)));
+    }
+
+    public function testSuiteRfcSha1Provider() {
+        $key_b32 = Base32::encode('12345678901234567890');
+        return [
+            [$key_b32,          59, '94287082'],
+            [$key_b32,  1111111109, '07081804'],
+            [$key_b32,  1111111111, '14050471'],
+            [$key_b32,  1234567890, '89005924'],
+            [$key_b32,  2000000000, '69279037'],
+            [$key_b32, 20000000000, '65353130'],
+        ];
+    }
+
+
+    public function testSuiteRfcSha256Provider() {
+        $key_b32 = Base32::encode('12345678901234567890123456789012');
+        return [
+            [$key_b32,          59, '46119246'],
+            [$key_b32,  1111111109, '68084774'],
+            [$key_b32,  1111111111, '67062674'],
+            [$key_b32,  1234567890, '91819424'],
+            [$key_b32,  2000000000, '90698825'],
+            [$key_b32, 20000000000, '77737706'],
+        ];
+    }
+
+    public function testSuiteRfcSha512Provider() {
+        $key_b32 = Base32::encode('1234567890123456789012345678901234567890123456789012345678901234');
+        return [
+            [$key_b32,          59, '90693936'],
+            [$key_b32,  1111111109, '25091201'],
+            [$key_b32,  1111111111, '99943326'],
+            [$key_b32,  1234567890, '93441116'],
+            [$key_b32,  2000000000, '38618901'],
+            [$key_b32, 20000000000, '47863826'],
+        ];
+    }
+
+    /**
+     * @dataProvider testSuiteRfcSha1Provider
+     */
+    public function testGenerateSha1($key_b32, $time, $expect) {
+        $this->assertEquals(
+            $expect,
+            Totp::calc(
+                $key_b32,
+                $time,
+                strlen($expect),
+                'sha1',
+                30
+            )
+        );
+    }
+
+    /**
+     * @dataProvider testSuiteRfcSha256Provider
+     */
+    public function testGenerateSha256($key_b32, $time, $expect) {
+        $this->assertEquals(
+            $expect,
+            Totp::calc(
+                $key_b32,
+                $time,
+                strlen($expect),
+                'sha256',
+                30
+            )
+        );
+    }
+
+    /**
+     * @dataProvider testSuiteRfcSha512Provider
+     */
+    public function testGenerateSha512($key_b32, $time, $expect) {
+        $this->assertEquals(
+            $expect,
+            Totp::calc(
+                $key_b32,
+                $time,
+                strlen($expect),
+                'sha512',
+                30
+            )
+        );
     }
 }
